32ce961d4d9480037f05925eb30a9c52d863ac61
Andrey525 <ghost5run@yandex.ru>
2021-11-29 20:32:26 +0700

    Try to change pipe

--- lab2/src/include/bebrash.h
+++ lab2/src/include/bebrash.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define BEBRASH_TOK_BUFSIZE 64
+#define BEBRASH_COMMAND_BUFSIZE 16
 #define BEBRASH_TOK_DELIM " \t\r\n\a"
 #define BEBRASH_COMMAND_DELIM "|"
 #define WRITE_END 1
@@ -13,7 +14,7 @@ char **bebrash_split_String_to_commands(char *String, int *count_commands);
 char **bebrash_split_command(char *command);
 int bebrash_launch(char **tokens);
 int bebrash_execute(char **tokens);
-int bebrash_launch_pipe(char **tokens, char **tokens2);
+int bebrash_launch_pipe(char ***tokens, int count_commands);
 
 int bebrash_cd(char **args);
 int bebrash_help(char **args);
--- lab2/src/src/bebrash.c
+++ lab2/src/src/bebrash.c
@@ -7,43 +7,50 @@
 #include <unistd.h>
 
 void greeting() {
-    printf("The bebrash command shell is running.\n");
-    printf("Enter your commands:\n");
+    printf("Командная оболочка Bebrash запущена.\n");
+    printf("Вводите команды:\n");
 }
 
 void bebrash_loop() {
     char *String;
     char **commands;
-    char **tokens;
-    char **tokens2;
+    char ***tokens;
     int status;
     int count_commands;
-    int i;
-    int fds1[2], fds2[2];
+    int i, j;
     do {
         printf("> ");
-        pipe(fds1);
-        pipe(fds2);
         String = bebrash_read_String();
         commands = bebrash_split_String_to_commands(String, &count_commands);
-        if (count_commands == 1) {
-            tokens = bebrash_split_command(commands[0]);
-            status = bebrash_execute(tokens);
-            free(String);
-            free(commands);
-            free(tokens);
-        } else if (count_commands == 2) {
-            tokens = bebrash_split_command(commands[0]);
-            tokens2 = bebrash_split_command(commands[1]);
-            status = bebrash_launch_pipe(tokens, tokens2);
-            free(String);
-            free(commands);
-            free(tokens);
-        } else {
-            printf("Sorry, Bebrash cannot execute more than 2 commands!\n");
-            free(String);
-            free(commands);
+        tokens = malloc(sizeof(char*) * count_commands);
+        for (i = 0; i < count_commands; i++) {
+            tokens[i] = bebrash_split_command(commands[i]);
+            j = 0;
+            // while (tokens[i][j] != NULL) {
+            //     printf("%s ", tokens[i][j]);
+            //     j++;
+            // }
+            // printf("\n");
         }
+        status = bebrash_launch_pipe(tokens, count_commands);
+        free(String);
+        free(commands);
+        free(tokens);
+        // if (count_commands == 1) {
+        //     status = bebrash_execute(tokens[0]);
+        //     free(String);
+        //     free(commands);
+        //     free(tokens);
+        // } else if (count_commands == 2) {
+        //     status = bebrash_launch_pipe(tokens, tokens2);
+        //     free(String);
+        //     free(commands);
+        //     free(tokens);
+        // } else {
+        //     printf("Bebrash не умеет выполнять более двух команд(((!\n");
+        //     free(String);
+        //     free(commands);
+        // }
     } while (status);
 }
 
@@ -55,7 +62,7 @@ char *bebrash_read_String() {
 }
 
 char **bebrash_split_String_to_commands(char *String, int *count_commands) {
-    int bufsize = 10;
+    int bufsize = BEBRASH_COMMAND_BUFSIZE;
     int i = 0;
     char **commands = malloc(bufsize * sizeof(char *));
     char *istr;
@@ -69,7 +76,7 @@ char **bebrash_split_String_to_commands(char *String, int *count_commands) {
         commands[i] = istr;
         i++;
         if (i == bufsize) {
-            bufsize += 10;
+            bufsize += BEBRASH_COMMAND_BUFSIZE;
             commands = realloc(commands, bufsize * sizeof(char *));
             if (!commands) {
                 fprintf(stderr, "bebrash: ошибка выделения памяти\n");
@@ -122,9 +129,6 @@ int bebrash_launch(char **tokens) {
             perror("exec");
         }
         exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        // Ошибка при форкинге
-        perror("fork");
     } else {
         // Родительский процесс
         do {
@@ -134,41 +138,64 @@ int bebrash_launch(char **tokens) {
     return 1;
 }
 
-int bebrash_launch_pipe(char **tokens, char **tokens2) {
-    pid_t pid;
-    int fd[2];
-    int status;
-    pipe(fd);
-    pid = fork();
-    if (pid == 0) {
-        dup2(fd[WRITE_END], STDOUT_FILENO);
-        close(fd[READ_END]);
-        close(fd[WRITE_END]);
-        execvp(tokens[0], tokens);
-        perror("exec1");
-        exit(1);
+int bebrash_launch_pipe(char ***tokens, int count_commands) {
+
+    if (count_commands == 1) {
+        int flag = bebrash_execute(tokens[0]);
+        if (flag != -1) {
+            return flag;
+        } else {
+            pid_t pid;
+            int status;
+            pid = fork();
+            if (pid == 0) {
+                // Дочерний процесс
+                if (execvp(tokens[0][0], tokens[0]) == -1) {
+                    perror("exec");
+                }   
+                exit(EXIT_FAILURE);
+            } else {
+                // Родительский процесс
+                do {
+                    waitpid(pid, &status, WUNTRACED);
+                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+            }
+            return 1;
+        }
+
     } else {
+        pid_t pid;
+        int fd[count_commands - 1][2];
+        int status;
+        int step = 0;
+        pipe(fd[0]);
         pid = fork();
         if (pid == 0) {
-            dup2(fd[READ_END], STDIN_FILENO);
-            close(fd[WRITE_END]);
-            close(fd[READ_END]);
-            execvp(tokens2[0], tokens2);
-            perror("exec2");
+            dup2(fd[0][WRITE_END], STDOUT_FILENO); // дублируем дескриптор вывода (теперь вывод идет не в stdout (мы его закрыли), а в fd[WRITE_END])
+            close(fd[0][READ_END]); // закрываем дескриптор для чтения
+            execvp(tokens[0][0], tokens[0]);
+            perror("exec1");
             exit(1);
         } else {
-            int status;
-            close(fd[READ_END]);
-            close(fd[WRITE_END]);
-            waitpid(pid, &status, 0);
+            pid = fork();
+            if (pid == 0) {
+                dup2(fd[0][READ_END], STDIN_FILENO); // дублируем дескриптор ввода (теперь ввод идет не из stdin (его мы к тому же закрыли), а из fd[READ_END])
+                close(fd[0][WRITE_END]); // закрываем дескриптор для вывода
+                execvp(tokens[1][0], tokens[1]);
+                perror("exec2");
+                exit(1);
+            } else {
+                int status;
+                close(fd[0][WRITE_END]); // закрываем дескриптор для вывода
+                waitpid(pid, &status, 0); // ждем завершения процессов
+            
+            }
         }
+        return 1;
     }
-    return 1;
+
 }
 
-/*
-  Список встроенных команд, за которыми следуют соответствующие функции
- */
 char *builtin_str[] = {"cd", "help", "exit"};
 
 int (*builtin_func[])(char **) = {&bebrash_cd, &bebrash_help, &bebrash_exit};
@@ -184,7 +211,7 @@ int bebrash_execute(char **tokens) {
             return (*builtin_func[i])(tokens);
         }
     }
-    return bebrash_launch(tokens);
+    return -1;
 }
 
 int bebrash_num_builtins() { return sizeof(builtin_str) / sizeof(char *); }
@@ -203,7 +230,7 @@ int bebrash_cd(char **args) {
 int bebrash_help(char **args) {
     int i;
     printf("Наберите название программы и её аргументы и нажмите enter.\n");
-    printf("Вот список втсроенных команд:\n");
+    printf("Вот список встроенных команд:\n");
     for (i = 0; i < bebrash_num_builtins(); i++) {
         printf("  %s\n", builtin_str[i]);
     }
